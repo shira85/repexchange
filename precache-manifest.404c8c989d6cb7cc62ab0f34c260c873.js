f239bffda93b216979291e3eb377f7930aa673dd
shira <m.shira2009@gmail.com>
2020-10-13 14:04:02 +0300

    Updates

---
+++ precache-manifest.404c8c989d6cb7cc62ab0f34c260c873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aceaec7501e08dadd21bf646530dd94",
+    "url": "/repexchange/index.html"
+  },
+  {
+    "revision": "c6da52291efd0b0252b4",
+    "url": "/repexchange/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f2c0325cc3c55cfa80c",
+    "url": "/repexchange/static/js/2.eef4a0b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/repexchange/static/js/2.eef4a0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6da52291efd0b0252b4",
+    "url": "/repexchange/static/js/main.ef44eb8d.chunk.js"
+  },
+  {
+    "revision": "1880e6697c165ff0db3a",
+    "url": "/repexchange/static/js/runtime-main.d7f91404.js"
+  }
+]);
